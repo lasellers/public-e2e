54c75f898ca43ee539a6f71dcaf20e2fc8690fd4
Lewis Sellers <lasellers@gmail.com>
2020-11-20 14:26:33 -0500

    Update menu tests

--- specs/form.spec.js
+++ specs/form.spec.js
@@ -7,7 +7,7 @@ fdescribe('form', function () {
 
   // $('#jb-radio-1').click()
   // $('input[type="radio"]:checked').val()
-  it('roundtrip', async function () {
+  it('One Way', async function () {
     //await element(by.id('jb-radio-1')).click();
     browser.actions().mouseMove(element(by.id('jb-radio-1'))).click().perform();
     let selected = element(by.css('input[type="radio"]:checked'));
@@ -17,12 +17,15 @@ fdescribe('form', function () {
 
   // $('#jb-radio-0').click()
   // $('input[type="radio"]:checked').val()
-  it('one way', async function () {
+  it('Roundtrip', async function () {
     // await element(by.id('jb-radio-0')).click();
     // await browser.actions().mouseMove(element(by.id('jb-radio-0'))).click().perform();
+
+    // Clicking the One-Way button first causes an issue where it covers the RT button we are trying to click
+    // so, we have to do a work-around that comes up on occasion to get by this -- I'd call it a bug -- with the 
+    // webdriverjs. We basically execute straight Javascript on the browser to force it to work.
     let clickScript = "document.getElementById('jb-radio-0').click();"
     await browser.driver.executeScript(clickScript).then(async function () {
-      browser.sleep(3000);
     });
 
     let selected = element(by.css('input[type="radio"]:checked'));
